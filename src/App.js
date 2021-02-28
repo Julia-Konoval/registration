d98fb0272f05e8b9226b263b19f5575f4812a3e9
Julia <yulyakonoval1998@gmail.com>
2021-02-28 21:34:24 +0200

    registration/login page created and styled

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { AuthProvider } from './contexts/AuthContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LoginHome from './components//login/LoginHome';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <PrivateRoute exact path="/" component={Home} />
+            <Route path="/login" component={LoginHome} />
+          </Switch>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
